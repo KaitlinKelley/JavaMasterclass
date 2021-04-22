7fc1276b98380078e1af6ef4fed98ba36fb15a3b
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-21 19:53:05 -0500

    worked on java masterclass

--- src/Hello.java
+++ src/Hello.java
@@ -14,12 +14,42 @@ public class Hello {
 //        //Line below results in an underflow
 //        System.out.println("Busted min value = " + (myMin -1));
 
-        byte b = 50;
-        short s = 14000;
-        int i = 17;
-        long l = (50000 + (10*(b + s + i)));
+//        byte b = 50;
+//        short s = 14000;
+//        int i = 17;
+//        long l = (50000 + (10*(b + s + i)));
+//
+//        System.out.println(l);
 
-        System.out.println(l);
+        //Ternary Operator Review===============================
+//        boolean isCar = false;
+//
+//        boolean wasCar = isCar ? true : false;
+//        isCar = true;
+//        if (wasCar){
+//            System.out.println("wasCar is true");
+//        }
+//
+//        int ageOfClient = 20;
+//        //if ageOfClient is 20, then assign true to isEighteenOrOver, otherwise assign false
+//        boolean isEighteenOrOver = (ageOfClient == 20) ? true : false;
+
+        //Operator Challenge===================================
+
+        double myDouble = 20.00;
+        double myOtherDouble = 80.00;
+
+        double myThirdDouble = (myDouble + myOtherDouble) * 100.00;
+
+        double myFourthDouble = myThirdDouble % 40.00;
+
+        boolean stepFive = (myFourthDouble == 0) ? true : false;
+
+        System.out.println(stepFive);
+
+        if(!stepFive){
+            System.out.println("Got some remainder.");
+        }
 
 
 
