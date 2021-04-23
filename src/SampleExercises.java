40003824a26000ccbe9b4fc22a16cc780a4b3b37
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-23 17:07:37 -0500

    worked on java masterclass

--- src/SampleExercises.java
+++ src/SampleExercises.java
@@ -1,7 +1,10 @@
 public class SampleExercises {
 
     public static void main(String[] args) {
-
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
     }
 
     //Speed Converter==============================================
@@ -46,6 +49,48 @@ public class SampleExercises {
         return true;
 
     };
+
+    //Leap Year Calculator==============================
+    public static boolean isLeapYear(int year){
+        if(year < 1 || year > 9999){
+            return false;
+        }else if(year % 4 != 0){
+            return false;
+        }else if(year % 100 != 0){
+            return true;
+        }else if(year % 400 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    //Decimal Comparator================================
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        int int1 = (int) (num1 * 1000);
+        int int2 = (int) (num2 * 1000);
+
+        return int1 == int2;
+    };
+
+    //Equal Sum Checker===============================
+    public static boolean hasEqualSum(int num1, int num2, int num3){
+        return num1 + num2 == num3;
+    };
+
+    //Teen Number Checker==============================
+    public static boolean hasTeen(int num1, int num2, int num3){
+        if((isTeen(num1)) || (isTeen(num2)) || (isTeen(num3))){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    public static boolean isTeen(int num1){
+        return ((num1 >= 13) && (num1 <= 19));
+    };
+
 
 
 
