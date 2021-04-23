4b940de8832f52dead5558bcbc895122a62dc56f
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-22 22:03:07 -0500

    worked on java masterclass

--- src/Hello.java
+++ src/Hello.java
@@ -36,24 +36,88 @@ public class Hello {
 
         //Operator Challenge===================================
 
-        double myDouble = 20.00;
-        double myOtherDouble = 80.00;
+//        double myDouble = 20.00;
+//        double myOtherDouble = 80.00;
+//
+//        double myThirdDouble = (myDouble + myOtherDouble) * 100.00;
+//
+//        double myFourthDouble = myThirdDouble % 40.00;
+//
+//        boolean stepFive = (myFourthDouble == 0) ? true : false;
+//
+//        System.out.println(stepFive);
+//
+//        if(!stepFive){
+//            System.out.println("Got some remainder.");
+//        }
 
-        double myThirdDouble = (myDouble + myOtherDouble) * 100.00;
+        //If-Else, Methods===============================================
 
-        double myFourthDouble = myThirdDouble % 40.00;
+//        boolean isMyGameOver = true;
+//        int myScore = 800;
+//        int myLevelCompleted = 5;
+//        int myBonus = 100;
+//
+//        int highScore = getFinalScore(isMyGameOver, myScore, myLevelCompleted, myBonus);
+//        System.out.println("Your final score was: " + highScore);
+//
+//        myScore = 10000;
+//        myLevelCompleted = 8;
+//        myBonus = 200;
+//
+//        highScore = getFinalScore(isMyGameOver, myScore, myLevelCompleted, myBonus);
+//        System.out.println("Your final score was: " + highScore);
 
-        boolean stepFive = (myFourthDouble == 0) ? true : false;
-
-        System.out.println(stepFive);
-
-        if(!stepFive){
-            System.out.println("Got some remainder.");
-        }
-
-
+        displayHighScorePosition("Foster", calculateHighScorePosition(1500));
+        displayHighScorePosition("Nimoy", calculateHighScorePosition(900));
+        displayHighScorePosition("Stanzi", calculateHighScorePosition(400));
+        displayHighScorePosition("Jon Snow", calculateHighScorePosition(50));
 
     }
-}
+
+
+
+    public static int getFinalScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            return finalScore;
+        }else{
+            return -1;
+        }
+    };
+
+    //A method that returns type void can also be known as a "procedure"
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println("Player " + name + " reached position: " + position);
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+//        if(playerScore >= 1000){
+//            return 1;
+//        }else if(playerScore >= 500){
+//            return 2;
+//        }else if(playerScore >= 100){
+//            return 3;
+//        }else{
+//            return 4;
+//        }
+
+
+        //Alternate solution with a single return statement
+        int pos = 4;
+        if(playerScore >= 1000){
+            pos = 1;
+        }else if(playerScore >= 500){
+            pos = 2;
+        }else if(playerScore >= 100){
+            pos = 3;
+        }
+        return pos;
+    };
+};
+
 
 //Expression - a construct that adds up to a single value
+
+
---
+++ src/SampleExercises.java
@@ -0,0 +1,41 @@
+public class SampleExercises {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Speed Converter==============================================
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            return -1;
+        }else{
+            return Math.round(kilometersPerHour/1.609);
+        }
+    };
+
+    public static void printConversion(double kilometersPerHour){
+        long result = toMilesPerHour(kilometersPerHour);
+        if(result == -1){
+            System.out.println("Invalid Value");
+        }else{
+            System.out.println(kilometersPerHour + " km/h = " + result + " mi/h");
+        }
+    };
+
+    //MegaBytes Converter=======================================
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }else{
+            int megaBytes = kiloBytes/1024;
+            int remKb = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remKb + " KB");
+        }
+    }
+
+
+
+
+
+}
