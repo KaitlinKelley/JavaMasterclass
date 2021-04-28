249ebddc3e787ecb6082d3a25975ce7c813c2fc1
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-28 14:38:05 -0500

    java masterclass

--- src/SampleExercises.java
+++ src/SampleExercises.java
@@ -14,9 +14,13 @@ public class SampleExercises {
 //        System.out.println(getDigitCount(-12));
 //        System.out.println(getDigitCount(123412));
 
-        System.out.println(reverse(-2));
-        numberToWords(0);
-        numberToWords(-100);
+//        System.out.println(reverse(-2));
+//        numberToWords(0);
+//        numberToWords(-100);
+
+//        System.out.println(canPack(2,2,12));
+
+        System.out.println(getLargestPrime(217));
 
 
 
@@ -482,6 +486,41 @@ public class SampleExercises {
     //Number to Words====================================
     public static void numberToWords(int number){
 
+        if(number >= 0 && number <= 9){
+            switch(number){
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+            }
+        }
+
         int newNum = reverse(number);
         int origDigCount = getDigitCount(number);
         int newDigCount = getDigitCount(newNum);
@@ -586,6 +625,60 @@ public class SampleExercises {
         } while(number > 0);
 
         return count;
+    }
+
+    //Flour Pack Problem
+
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        if(bigCount < 0 || smallCount < 0 || goal < 0){
+            return false;
+        }
+
+        int possibleKilos = bigCount*5 + smallCount;
+
+        if(goal > possibleKilos){
+            return false;
+        }
+        if(goal == possibleKilos){
+            return true;
+        }
+
+        if(bigCount == 0 && smallCount >= goal){
+            return true;
+        }else{
+
+        }
+
+
+        if((bigCount * 5) % goal != smallCount){
+            return false;
+        }
+
+        return true;
+    }
+
+    //Largest Prime
+
+    public static int getLargestPrime(int number){
+        if(number < 2){
+            return -1;
+        }
+
+        int currentPrime = -1;
+
+        for(int j = 2; j <= number; j++){
+            for(int i=2; i<= (long) Math.sqrt(j); i++){
+                if(j % i != 0){
+                    currentPrime = j;
+                }
+            }
+        }
+
+
+
+        return currentPrime;
+
+
     }
 
 
