d65e55db883d5d9a09b0e82efbb32624ed8ee1dd
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-05-04 16:18:06 -0500

    reviewed static vs instance, inheritance challenge

--- src/Car.java
+++ src/Car.java
@@ -1,23 +1,32 @@
-public class Car {
+public class Car extends Vehicle{
 
     private int doors;
     private int wheels;
-    private String model;
-    private String engine;
-    private String color;
+    private int gears;
+    private boolean isManual;
+
+    private int currentGear;
 
     //Custom classes MUST be initialized with "new", for they have no default value
 
 
-
-
-
-
-
-    public static void main(String[] args) {
-        Car porsche = new Car();
-        Car holden = new Car();
+    public Car(String name, String size, int doors, int wheels, int gears, boolean isManual) {
+        super(name, size);
+        this.doors = doors;
+        this.wheels = wheels;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
     }
 
+    public void changeGear(int gear){
+        this.currentGear = gear;
+        System.out.println("Car.setCurrentGear(): Changed to " + gear + " gear");
+    }
+
+    public void changeVelocity(int speed, int direction){
+        move(speed, direction);
+        System.out.println("Car.changeVelocity(): Velocity " + speed + ", direction " + direction);
+    }
 
 }
