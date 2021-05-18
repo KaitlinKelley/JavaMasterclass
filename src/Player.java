4ed35575d1aab61ec9b375b37bab8b1e2ecd0cea
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-05-17 21:10:33 -0500

    interface practice

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Player implements ISaveable{
 
@@ -76,5 +77,64 @@ public class Player implements ISaveable{
             this.strength = Integer.parseInt(values.get(2));
             this.weapon = values.get(3);
         }
+    }
+
+    public static ArrayList<String> readValues(){
+        ArrayList<String> values = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose:");
+        System.out.println("1 to enter a string");
+        System.out.println("0 to quit");
+
+        while(!quit){
+            System.out.println("Choose an option:");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a string:");
+                    String input = sc.nextLine();
+                    values.add(index, input);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    //will accept any object that implements ISaveable
+    public static void saveObject(ISaveable objectToSave){
+        for (int i= 0; i<objectToSave.write().size(); i++){
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device.");
+
+        }
+    }
+
+    //will accept any object that implements ISaveable
+    public static void loadObject(ISaveable objectToLoad){
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+
+    public static void main(String[] args) {
+        Player tim = new Player("Tim", 10, 15);
+        System.out.println(tim.toString());
+        saveObject(tim);
+
+        tim.setHitPoints(8);
+        System.out.println(tim);
+        tim.setWeapon("Stormbringer");
+        saveObject(tim);
+        System.out.println(tim);
+
+        Monster werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println(werewolf);
+        saveObject(werewolf);
     }
 }
